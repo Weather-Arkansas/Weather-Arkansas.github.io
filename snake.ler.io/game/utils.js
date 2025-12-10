5198188b5d97e7f4f871ba999243e736f057ed78
eclipsed <duncanhomeschool@gmail.com>
2025-12-10 12:02:34 -0600

    Delete snake.ler.io directory

--- snake.ler.io/game/utils.js
+++
@@ -1,8 +0,0 @@
-// utils.js - Utility functions like random food generation, etc.
-
-function getRandomFoodPosition() {
-    return {
-        x: Math.floor(Math.random() * 50) * 10,
-        y: Math.floor(Math.random() * 50) * 10,
-    };
-}
